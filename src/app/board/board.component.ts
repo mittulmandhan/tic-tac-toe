490ba59f711f36df932a79133e4b19c819aa8f21
mittulmandhan <conanmittul@gmail.com>
2020-03-28 20:12:22 +0530

    board component complete

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,10 +6,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    // START NEW GAME
+    this.newGame();
+  }
+
+  // SETUP A NEW GAME
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  // MOVE
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+    this.winner = this.calculateWinner();
+  }
+
+
+  // CALCULATE WINNER
+  calculateWinner() {
+    /*Board
+          0  1  2
+          3  4  5
+          6  7  8
+    */
+
+    // all possible lines
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    // CHECK IF THE LINE IS FORM
+    // EITHER THROUGH 'X' OR 'O'
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
   }
 
 }
